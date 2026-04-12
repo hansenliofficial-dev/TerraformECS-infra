f398fa77b6a1ca7b56af81e8f9cecec13216a563
Hansen <hansenliofficial@gmail.com>
2026-04-12 17:34:00 +0200

    Change app_name to match updated application

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "app_name" {
   description = "Application name used for resource naming"
   type        = string
-  default     = "hello-world"
+  default     = "gtiapp"
 }
 
 variable "github_owner" {
